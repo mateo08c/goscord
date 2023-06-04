d4e1e49836db224be1a9a1d43313cfe4da43127d
mateo08c <36140949+mateo08c@users.noreply.github.com>
2023-06-04 17:09:51 +0200

    style: remove debug log

--- goscord/gateway/invite_handler.go
+++ goscord/gateway/invite_handler.go
@@ -2,15 +2,12 @@ package gateway
 
 import (
 	"github.com/Goscord/goscord/goscord/gateway/event"
-	"log"
 )
 
 type InviteCreateHandler struct{}
 
 func (_ *InviteCreateHandler) Handle(s *Session, data []byte) {
 	ev, err := event.NewInviteCreate(s.rest, data)
-
-	log.Println(ev)
 
 	if err != nil {
 		return
